92b5376d4aae019622e3858764ee837e37337b39
Alferdiyan <ferdi1505@gmail.com>
2023-03-26 09:16:01 +0700

    change size

--- components/BakgroundCircle.tsx
+++ components/BakgroundCircle.tsx
@@ -4,11 +4,11 @@ import { motion } from 'framer-motion';
 type Props = {};
 
 export default function BakgroundCircle({}: Props) {
-    return (
-        <div className="relative flex justify-center items-center text-center">
-            <div className="rounded-full border border-[#333333] h-[250px] w-[250px] absolute mt-52" />
-            <div className="rounded-full border border-[#333333] h-[550px] w-[550px] absolute mt-52 animate-pulse" />
-            <div className="rounded-full border border-[#f7ab0a] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse" />
-        </div>
-    );
+  return (
+    <div className="relative flex justify-center items-center text-center">
+      <div className="rounded-full border border-[#333333] h-[450px] w-[450px] absolute mt-52" />
+      <div className="rounded-full border border-[#333333] h-[550px] w-[550px] absolute mt-52 animate-pulse" />
+      <div className="rounded-full border border-[#f7ab0a] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse" />
+    </div>
+  );
 }
