ecad9ceba012e2c6fa5786c3f950587c85887a3c
Alferdiyan <ferdi1505@gmail.com>
2023-03-26 09:13:46 +0700

    change size backgorund circle

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,46 +1,15 @@
 import React from 'react';
-import ContactForm from './ContactForm';
 
 export default function ContactMe() {
-    return (
-        <div className="m-20 h-screen">
-            <div className="top-24 uppercase tracking-[20px] text-gray-500 text-2xl text-center m-10">
-                <h1>Contact</h1>
-            </div>
-            <div className="m-10 p-5 flex flex-wrap ">
-                <div className="w-full md:w-1/2 lg:w-1/2 flex flex-wrap justify-center items-center mb-4">
-                    <div className="w-full   text-center">
-                        <div className="items-center mb-2 text-white">
-                            <p>Our Address</p>
-                        </div>
-                        <p className="ml-2 font-bold text-sm text-white">
-                            Jl. Pandawa No. 146, Sukoharjo, Central Java,
-                            Indonesia
-                        </p>
-                    </div>
-                    <div className="w-full   text-center">
-                        <div className="items-center mb-2 text-white">
-                            <p>Email Us</p>
-                        </div>
-                        <p className="ml-2 font-bold text-sm text-white">
-                            Ferdiyan.eka1@gmail.com
-                        </p>
-                    </div>
-                    <div className="w-full   text-center">
-                        <div className="items-center mb-2 text-white">
-                            <p>Our Contact</p>
-                        </div>
-                        <p className="ml-2 font-bold text-sm text-white">
-                            +62813 9295 5870
-                        </p>
-                    </div>
-                </div>
-
-                {/* contak form */}
-                <div className="w-full  md:w-1/2 lg:w-1/2">
-                    <ContactForm />
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="h-3/5">
+      <div className="top-24 uppercase tracking-[2px] text-gray-400 text-2xl flex justify-center items-center text-center m-10">
+        <p>need a front end developer</p>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-7xl font-semibold">Lets Work Together</p>
+        <p className=" text-xs p-10 text-white">Feel free to reach out to me any time. I prefer to talk over email, especially since we may be a few time zones away.</p>
+      </div>
+    </div>
+  );
 }
