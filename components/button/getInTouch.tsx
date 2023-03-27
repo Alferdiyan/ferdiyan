fe2349dc31378f58c59032079d1e772ccde9d7f2
Alferdiyan <ferdi1505@gmail.com>
2023-03-27 11:30:32 +0700

    add button

--- components/button/getInTouch.tsx
+++ components/button/getInTouch.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function GetInTouch() {
   return (
-    <div className="border  px-4 py-2 rounded-full hover:bg-orange-400">
+    <div className="border  px-4 py-2 rounded-full hover:bg-orange-500">
       <Link href="#">
         <button>Get In Touch</button>
       </Link>
