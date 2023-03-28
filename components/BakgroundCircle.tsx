8d4b596b32afdecc1bf49212669cf3b15d1a8770
Alferdiyan <ferdi1505@gmail.com>
2023-03-28 09:59:40 +0700

    add responsive

--- components/BakgroundCircle.tsx
+++ components/BakgroundCircle.tsx
@@ -6,9 +6,9 @@ type Props = {};
 export default function BakgroundCircle({}: Props) {
   return (
     <div className="relative flex justify-center items-center text-center">
-      <div className="rounded-full border border-[#333333] h-[450px] w-[450px] absolute mt-52" />
-      <div className="rounded-full border border-[#333333] h-[550px] w-[550px] absolute mt-52 animate-pulse" />
-      <div className="rounded-full border border-[#f7ab0a] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse" />
+      <div className="rounded-full border border-[#f7ab0a] h-[450px] w-[450px] absolute mt-52 animate-pulse" />
+      <div className="rounded-full border border-[#333333] h-[550px] w-[550px] hidden md:flex absolute mt-52 animate-pulse " />
+      <div className="rounded-full border border-[#f7ab0a] opacity-20 h-[650px] w-[650px] hidden md:flex absolute mt-52 animate-pulse" />
     </div>
   );
 }
