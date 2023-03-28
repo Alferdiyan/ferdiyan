36afbeb77d0c33f7879c86b1e62a6978580a415d
Alferdiyan <ferdi1505@gmail.com>
2023-03-28 10:01:27 +0700

    add navbar

--- components/NavScrolled.tsx
+++ components/NavScrolled.tsx
@@ -24,9 +24,10 @@ function NavScrolled() {
     setIsNavVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
     setPrevScrollPos(currentScrollPos);
   };
+  const [open, setOpen] = useState(false);
 
   return (
-    <nav style={{ transform: isNavVisible ? 'translateY(0)' : 'translateY(-100%) ' }} className="px-2 pt-3 flex items-center justify-evenly z-100">
+    <nav style={{ transform: isNavVisible ? 'translateY(0)' : 'translateY(-100%) ' }} className="px-2 pt-3 hidden md:flex items-center justify-evenly z-100">
       <a href="#">Ferdiyan</a>
       <ul className={`flex gap-4 justify-center `}>
         {menuItems.map((item) => (
@@ -41,38 +42,3 @@ function NavScrolled() {
 }
 
 export default NavScrolled;
-
-// import React, { useState, useEffect } from 'react';
-
-// function NavScrolled() {
-//   const [isScrolled, setIsScrolled] = useState(false);
-
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       const scrollPosition = window.scrollY;
-//       if (scrollPosition > 0) {
-//         setIsScrolled(true);
-//       } else {
-//         setIsScrolled(false);
-//       }
-//     };
-
-//     window.addEventListener('scroll', handleScroll);
-
-//     return () => {
-//       window.removeEventListener('scroll', handleScroll);
-//     };
-//   }, []);
-
-//   return (
-//     <nav className={`fixed top-0 left-0 right-0 z-100 bg-white shadow-md transition-all duration-500 ${isScrolled ? 'py-2' : 'py-5'} ${isScrolled ? 'opacity-100' : 'opacity-0'} `}>
-//       <div className="container mx-auto px-4">
-//         <a href="#" className="text-lg font-bold text-white hover:text-gray-100">
-//           My Site
-//         </a>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default NavScrolled;
