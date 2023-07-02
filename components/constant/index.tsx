c013d1acd8dd61317567ebbad31f063ecf7d3d77
alferdiyan <ferdi1505@gmail.com>
2023-07-02 17:32:52 +0700

    change card data

---
+++ components/constant/index.tsx
@@ -0,0 +1,9 @@
+export const profile = [
+  {
+    id: 'hero',
+    name: 'Eka Ferdiyanto',
+    role: 'Graphic Designer dan UI Designer',
+    description:
+      'Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.',
+  },
+];
