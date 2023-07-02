071e5abc826dc6c5417597bad34784d8e4f6d5f5
alferdiyan <ferdi1505@gmail.com>
2023-07-02 16:10:07 +0700

    edit config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,31 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#f97316',
+
+          secondary: '#bff9fc',
+
+          accent: '#9082e0',
+
+          neutral: '#211c30',
+
+          'base-100': '#ffffff',
+
+          info: '#324fd2',
+
+          success: '#1fe59c',
+
+          warning: '#f1c346',
+
+          error: '#fd645e',
+        },
+      },
+      'bumblebee',
+    ],
+  },
   plugins: [require('daisyui')],
 };
