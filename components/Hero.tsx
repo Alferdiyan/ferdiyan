aa86d89464b17cd8c7d039ec019a2a276c335f4c
alferdiyan <ferdi1505@gmail.com>
2023-07-04 07:22:34 +0700

    modife hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 import photo from '../public/image/Eka_ferdiyanto.png';
 import GetInTouch from './button/GetInTouch';
+import blob from '@/public/blob.svg';
 
 type Props = {
   name: string;
@@ -14,17 +15,19 @@ const Hero: React.FC<Props> = ({ name, role, description }) => {
     <div>
       <div className="hero min-h-screen bg-base-100">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <Image
-            src={photo}
-            alt="hero"
-            width={600}
-            className="max-w-xs rounded-full sm:max-w-xl  bg-transparent"
-          />
+          <div className="">
+            <Image
+              src={photo}
+              alt="hero"
+              width={600}
+              className="top-0 z-10 max-w-xs md:max-w-xl  rounded-full  bg-transparent"
+            />
+          </div>
           <div>
             <p className="text-primary text-lg font-semibold">{name}</p>
             <h1 className="text-5xl font-bold">{role}</h1>
             <p className="py-6">{description}</p>
-            <div className="gap-2 flex items-center">
+            <div className="gap-2 flex items-center ">
               <GetInTouch />
 
               <p>or</p>
