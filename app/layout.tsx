5cda64a44172ac208f3a052a5677e7ba3cf07eb2
alferdiyan <ferdi1505@gmail.com>
2023-07-08 09:10:00 +0700

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,12 @@
-'use client';
 import Navbar from '@/components/Navbar';
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { SocialIcon } from 'react-social-icons';
-import { motion } from 'framer-motion';
-import Logo from '@/public/ferdiyan.svg';
-import Image from 'next/image';
+import Footer from '@/components/Footer';
 
-const inter = Inter({ subsets: ['latin'] });
-
-const socialIcons = [
-  {
-    url: 'https://www.youtube.com/ferdiyan',
-    icon: 'youtube',
-  },
-  {
-    url: 'https://github.com/Alferdiyan',
-    icon: 'github',
-  },
-  {
-    url: 'https://www.linkedin.com/in/eka-ferdiyanto-11ab551b2/',
-    icon: 'linkedin',
-  },
-  {
-    url: 'https://www.instagram.com/ferdiyan.id',
-    icon: 'instagram',
-  },
-];
+export const metadata = {
+  title: 'Eka Ferdiyanto | UI and Web Developer ',
+  description: 'UI Web Designer',
+};
 
 export default function RootLayout({
   children,
@@ -34,48 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="bg-base" data-theme="mytheme">
-      <body className={`${inter.className} overflow-x-hidden`}>
-        <section className="px-5 sm:px-10 md:px-20">
-          <Navbar />
-          {children}
-        </section>
-        <footer className="footer   items-center p-4 bg-primary text-neutral-content">
-          <div className="items-center grid-flow-col">
-            <Image src={Logo} alt="logo" width={40} height={40} />
-            <p>Copyright Ferdiyan© 2023 - All right reserved</p>
-          </div>
-          <div className="grid-flow-col gap-4 place-self-center md:justify-self-end">
-            <motion.div
-              initial={{
-                x: 500,
-                opacity: 0,
-                scale: 0.5,
-              }}
-              animate={{
-                x: 0,
-                opacity: 1,
-                scale: 1,
-              }}
-              transition={{
-                duration: 1.5,
-              }}
-              className="flex flex-row items-center"
-            >
-              {socialIcons.map((icon, index) => (
-                <SocialIcon
-                  key={index}
-                  url={icon.url}
-                  fgColor="white"
-                  bgColor="transparent"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                />
-              ))}
-            </motion.div>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <div className="bg-base" data-theme="mytheme">
+      <section className="px-5 sm:px-10 md:px-20">
+        <Navbar />
+        {children}
+      </section>
+      <Footer />
+    </div>
   );
 }
