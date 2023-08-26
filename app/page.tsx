f77ab9e47e0efd3b023ddd98ab8904e455827592
alferdiyan <ferdi1505@gmail.com>
2023-08-26 22:58:38 +0700

    delete Props

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,6 @@ import Service from "@/components/Service";
 import ContactMe from "@/components/ContactMe";
 import Project from "@/components/Project";
 
-export async function getServerSideProps() {
-    const apiUrl = process.env.APIURL;
-    console.log(process.env.APIURL);
-    const req = await fetch(`${apiUrl}/api/posts`);
-    const res = await req.json();
-
-    console.log(res);
-
-    return {
-        prop: {},
-    };
-}
-
 export default function Home() {
     return (
         <main>
