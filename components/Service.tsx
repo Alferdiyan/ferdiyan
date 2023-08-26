3affef43c6d36ff1a965373a419f1043ede7b096
alferdiyan <ferdi1505@gmail.com>
2023-08-27 00:37:48 +0700

    delete getseversideprops

--- components/Service.tsx
+++ components/Service.tsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import ServicesCard from './ServiceCard';
-import mobileDev from '../public/mobileDev.png';
-import webDev from '../public/webDev.png';
-import uiUX from '../public/uiUX.png';
-import { getServerSideProps } from '../app/page';
+import React from "react";
+import ServicesCard from "./ServiceCard";
+import mobileDev from "../public/mobileDev.png";
+import webDev from "../public/webDev.png";
+import uiUX from "../public/uiUX.png";
 
 const servicesData = [
-  {
-    judul: 'Mobile Developer',
-    description:
-      'We design mobile app designs with the latest trends and of course according to your needs',
-    logo: mobileDev,
-  },
-  {
-    judul: 'Web Developer',
-    description:
-      'The website is like a house and it is appropriate to give it a good look and according to its function.',
-    logo: webDev,
-  },
-  {
-    judul: 'UI Design',
-    description: 'Intuitive, easy to use, and visually appealing for the user',
-    logo: uiUX,
-  },
+    {
+        judul: "Mobile Developer",
+        description:
+            "We design mobile app designs with the latest trends and of course according to your needs",
+        logo: mobileDev,
+    },
+    {
+        judul: "Web Developer",
+        description:
+            "The website is like a house and it is appropriate to give it a good look and according to its function.",
+        logo: webDev,
+    },
+    {
+        judul: "UI Design",
+        description:
+            "Intuitive, easy to use, and visually appealing for the user",
+        logo: uiUX,
+    },
 ];
 
 export default function Service(): JSX.Element {
-  return (
-    <div className="h-max py-8">
-      <div className="flex justify-center uppercase tracking-[20px] text-primary text-2xl mt-10 m-30">
-        Provide Services
-      </div>
-      <div className="flex flex-wrap gap-4 my-20 justify-center">
-        {servicesData.map((service, index) => (
-          <ServicesCard
-            key={index}
-            judul={service.judul}
-            description={service.description}
-            logo={service.logo}
-          />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="h-max py-8">
+            <div className="flex justify-center uppercase tracking-[20px] text-primary text-2xl mt-10 m-30">
+                Provide Services
+            </div>
+            <div className="flex flex-wrap gap-4 my-20 justify-center">
+                {servicesData.map((service, index) => (
+                    <ServicesCard
+                        key={index}
+                        judul={service.judul}
+                        description={service.description}
+                        logo={service.logo}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
