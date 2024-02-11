c949ef3f775af785ec6e3f27531df19ee3c247b2
alferdiyan <ferdi1505@gmail.com>
2024-02-11 07:35:38 +0700

    deactivate blog component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FileSearch } from "@phosphor-icons/react";
+// import { FileSearch } from "@phosphor-icons/react";
 // import { FileSearch, MagnifyingGlass } from "@phosphor-icons/react";
 import Link from "next/link";
 import React from "react";
@@ -11,7 +11,7 @@ export default function Page() {
                 className="flex justify-center items-center gap-2 
             "
             >
-                <FileSearch size={32} />
+                {/* <FileSearch size={32} /> */}
                 <p className="text-color-accent font-bold uppercase">
                     not-found
                 </p>
--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
                 {/* <Navigation /> */}
                 <Service />
                 <Project />
-                <Blogs />
+                {/* <Blogs /> */}
                 <ContactMe />
                 <Footer />
             </div>
